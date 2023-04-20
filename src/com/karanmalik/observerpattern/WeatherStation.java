03ead1bbcb391d5be815078cf98eabcb2f7f8adf
Karan Malik <97101066+karanmalikkm@users.noreply.github.com>
2023-04-21 00:52:53 +0530

    Adding HeatIndexDisplay.java to include HeatindexDisplay observer as a new observer

--- src/com/karanmalik/observerpattern/WeatherStation.java
+++ src/com/karanmalik/observerpattern/WeatherStation.java
@@ -1,12 +1,14 @@
 package com.karanmalik.observerpattern;
 
 import com.karanmalik.observerpattern.observers.CurrentConditionsDisplay;
+import com.karanmalik.observerpattern.observers.HeatIndexDisplay;
 import com.karanmalik.observerpattern.subject.WeatherData;
 
 public class WeatherStation {
     public static void showWeatherData(){
         WeatherData weatherData = new WeatherData();
         CurrentConditionsDisplay currentConditionsDisplay = new CurrentConditionsDisplay(weatherData);
+        HeatIndexDisplay heatIndexDisplay = new HeatIndexDisplay(weatherData);
         weatherData.setMeasurements(80,64, 30.4f);
     }
 }
